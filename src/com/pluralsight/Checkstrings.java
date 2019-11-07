60efafb2bc9b33594e6a079a8339bc102255bb0d
waquar <waquar.alam92@gmail.com>
2019-11-07 14:07:43 +0530

    added

---
+++ src/com/pluralsight/Checkstrings.java
@@ -0,0 +1,269 @@
+package com.pluralsight;
+
+public class Checkstrings {
+    public String[] stations = {"Adarsh Nagar\n" +
+            "  \tAIIMS\n" +
+            "  \tAkshardham\n" +
+            "  \tAlpha 1 Greater Noida\n" +
+            "  \tAnand Vihar\n" +
+            "  \tAnand Vihar\n" +
+            "  \tArjan Garh\n" +
+            "  \tArthala\n" +
+            "  \tAshok Park Main\n" +
+            "  \tAshram\n" +
+            "  \tAzadpur\n" +
+            "  \tBadarpur\n" +
+            "  \tBadkal Mor\n" +
+            "  \tBahdurgarh City\n" +
+            "  \tBarakhamba\n" +
+            "  \tBata Chowk\n" +
+            "  \tBelvedere Towers Vodaphone\n" +
+            "  \tBhikaji Cama Place\n" +
+            "  \tBotanical Garden\n" +
+            "  \tBrig Hoshiar Singh\n" +
+            "  \tCentral Secretariat\n" +
+            "  \tChandni Chowk\n" +
+            "  \tChawri Bazar\n" +
+            "  \tChhattarpur\n" +
+            "  \tChirag Delhi\n" +
+            "  \tCivil Lines\n" +
+            "  \tCyber City Indus Bank\n" +
+            "  \tDabri Mor\n" +
+            "  \tDashrath Puri\n" +
+            "  \tDelhi Aero City\n" +
+            "  \tDelhi Cantt\n" +
+            "  \tDelhi Gate\n" +
+            "  \tDelta 1 Greater Noida\n" +
+            "  \tDepot Greater Noida\n" +
+            "  \tDhaula Kuan\n" +
+            "  \tDilshad Garden\n" +
+            "  \tDLF Phase 1\n" +
+            "  \tDLF Phase 2\n" +
+            "  \tDLF Phase 3\n" +
+            "  \tDurgabai Deshmukh South Campus\n" +
+            "  \tDwarka\n" +
+            "  \tDwarka Mor\n" +
+            "  \tDwarka Sector 8\n" +
+            "  \tDwarka Sector 9\n" +
+            "  \tDwarka Sector 10\n" +
+            "  \tDwarka Sector 11\n" +
+            "  \tDwarka Sector 12\n" +
+            "  \tDwarka Sector 13\n" +
+            "  \tDwarka Sector 14\n" +
+            "  \tDwarka Sector 21\n" +
+            "  \tEast Azad Nagar\n" +
+            "  \tEscorts Mujesar\n" +
+            "  \tESI Hospital\n" +
+            "  \tG.T.B. Nagar\n" +
+            "  \tGhevra Metro station\n" +
+            "  \tGhitorni\n" +
+            "  \tGNIDA Office\n" +
+            "  \tGokulpuri\n" +
+            "  \tGolf Course\n" +
+            "  \tGovind Puri\n" +
+            "  \tGreater Kailash\n" +
+            "  \tGreen Park\n" +
+            "  \tGuru Dronacharya\n" +
+            "  \tHaiderpur Badli Mor\n" +
+            "  \tHauz Khas\n" +
+            "  \tHazrat Nizamuddin\n" +
+            "  \tHindon\n" +
+            "  \tHuda City Centre\n" +
+            "  \tIIT Delhi\n" +
+            "  \tIFFCO Chowk\n" +
+            "  \tIGI Airport\n" +
+            "  \tINA\n" +
+            "  \tInder Lok\n" +
+            "  \tIndraprastha\n" +
+            "  \tIP Extension\n" +
+            "  \tITO\n" +
+            "  \tJaffrabad\n" +
+            "  \tJahangirpuri\n" +
+            "  \tJama Masjid\n" +
+            "  \tJamia Millia Islamia\n" +
+            "  \tJanak Puri East\n" +
+            "  \tJanak Puri West\n" +
+            "  \tJangpura\n" +
+            "  \tJanpath\n" +
+            "  \tJasola\n" +
+            "  \tJasola Vihar Shaheen Bagh\n" +
+            "  \tJhandewalan\n" +
+            "  \tJhil Mil\n" +
+            "  \tJawaharlal Nehru Stadium\n" +
+            "  \tJohri Enclave\n" +
+            "  \tJorbagh\n" +
+            "  \tKailash Colony\n" +
+            "  \tKalindi Kunj\n" +
+            "  \tKalkaji Mandir\n" +
+            "  \tKanhaiya Nagar\n" +
+            "  \tKarkar Duma\n" +
+            "  \tKarkarduma Court\n" +
+            "  \tKarol Bagh\n" +
+            "  \tKashmere Gate\n" +
+            "  \tKaushambi\n" +
+            "  \tKeshav Puram\n" +
+            "  \tKhan Market\n" +
+            "  \tKirti Nagar\n" +
+            "  \tKnowledge Park II\n" +
+            "  \tKohat Enclave\n" +
+            "  \tKrishna Nagar\n" +
+            "  \tLajpat Nagar\n" +
+            "  \tLal Quila\n" +
+            "  \tLaxmi Nagar\n" +
+            "  \tLok Kalyan Marg\n" +
+            "  \tMG Road\n" +
+            "  \tMadi Pur\n" +
+            "  \tMajlis Park\n" +
+            "  \tMajor Mohit Sharma\n" +
+            "  \tMalviya Nagar\n" +
+            "  \tMandawali - West Vinod Nagar\n" +
+            "  \tMandi House\n" +
+            "  \tMansarovar Park\n" +
+            "  \tMaujpur\n" +
+            "  \tMaya Puri\n" +
+            "  \tMayur Vihar Pocket I\n" +
+            "  \tMayur Vihar Phase-1\n" +
+            "  \tMayur Vihar Extention\n" +
+            "  \tMewala Maharajpur\n" +
+            "  \tModel Town\n" +
+            "  \tMohan Estate\n" +
+            "  \tMohan Nagar\n" +
+            "  \tMoolchand\n" +
+            "  \tMoti Nagar\n" +
+            "  \tMicromax Moulsari\n" +
+            "  \tMundka\n" +
+            "  \tMundka Industrial Area (MIA)\n" +
+            "  \tMunirka\n" +
+            "  \tNangloi\n" +
+            "  \tNangloi Rly Station\n" +
+            "  \tNaraina Vihar\n" +
+            "  \tNawada\n" +
+            "  \tNeelam Chowk Ajronda\n" +
+            "  \tNehru Enclave\n" +
+            "  \tNehru Place\n" +
+            "  \tNetaji Subhash Place\n" +
+            "  \tNew Ashok Nagar\n" +
+            "  \tNew Delhi\n" +
+            "  \tNew Delhi-Airport Express\n" +
+            "  \tN.H.P.C. Chowk\n" +
+            "  \tNirman Vihar\n" +
+            "  \tNoida City Center\n" +
+            "  \tNoida Electronic City\n" +
+            "  \tNoida Sector 15\n" +
+            "  \tNoida Sector 16\n" +
+            "  \tNoida Sector 18\n" +
+            "  \tNoida Sector 34\n" +
+            "  \tNoida Sector 50\n" +
+            "  \tNoida Sector 51\n" +
+            "  \tNoida Sector 52\n" +
+            "  \tNoida Sector 59\n" +
+            "  \tNoida Sector 61\n" +
+            "  \tNoida Sector 62\n" +
+            "  \tNoida Sector 76\n" +
+            "  \tNoida Sector 81\n" +
+            "  \tNoida Sector 83\n" +
+            "  \tNoida Sector 101\n" +
+            "  \tNoida Sector 137\n" +
+            "  \tNoida Sector 142\n" +
+            "  \tNoida Sector 143\n" +
+            "  \tNoida Sector 144\n" +
+            "  \tNoida Sector 145\n" +
+            "  \tNoida Sector 146\n" +
+            "  \tNoida Sector 147\n" +
+            "  \tNoida Sector 148\n" +
+            "  \tNSEZ Noida\n" +
+            "  \tOkhla N.S.I.C\n" +
+            "  \tOkhla\n" +
+            "  \tOkhla Bird Sanctuary\n" +
+            "  \tOkhla Vihar\n" +
+            "  \tOld Faridabad\n" +
+            "  \tPalam\n" +
+            "  \tPanchsheel Park\n" +
+            "  \tPandit Shree Ram Sharma\n" +
+            "  \tPari Chowk Greater Noida\n" +
+            "  \tPaschim Vihar (East)\n" +
+            "  \tPaschim Vihar (West)\n" +
+            "  \tPatel Chowk\n" +
+            "  \tPatel Nagar\n" +
+            "  \tPeera Garhi\n" +
+            "  \tPitam Pura\n" +
+            "  \tPragati Maidan\n" +
+            "  \tPratap Nagar\n" +
+            "  \tPreet Vihar\n" +
+            "  \tPul Bangash\n" +
+            "  \tPunjabi Bagh\n" +
+            "  \tPunjabi Bagh West\n" +
+            "  \tQutab Minar\n" +
+            "  \tR.K Puram\n" +
+            "  \tRaj Bagh\n" +
+            "  \tRaja Nahar Singh\n" +
+            "  \tRajdhani Park\n" +
+            "  \tRajendra Place\n" +
+            "  \tRajiv Chowk\n" +
+            "  \tRajouri Garden\n" +
+            "  \tRamesh Nagar\n" +
+            "  \tRithala\n" +
+            "  \tR K Ashram Marg\n" +
+            "  \tRohini East\n" +
+            "  \tRohini Sector 18,19\n" +
+            "  \tRohini West\n" +
+            "  \tSadar Bazaar Cantonment\n" +
+            "  \tSaket\n" +
+            "  \tSamaypur Badli\n" +
+            "  \tSant Surdas - Sihi\n" +
+            "  \tSarai\n" +
+            "  \tSarita Vihar\n" +
+            "  \tSarojini Nagar\n" +
+            "  \tSatguru Ram Singh Marg\n" +
+            "  \tSector 42-43\n" +
+            "  \tSector 53-54\n" +
+            "  \tSector 54 Chowk\n" +
+            "  \tSector 55-66\n" +
+            "  \tSector 28 Faridabad\n" +
+            "  \tSeelampur\n" +
+            "  \tShadipur\n" +
+            "  \tShahdara\n" +
+            "  \tShaheed Nagar\n" +
+            "  \tShaheed Sthal(New Bus Adda)\n" +
+            "  \tShakurpur\n" +
+            "  \tShalimar Bagh\n" +
+            "  \tShankar Vihar\n" +
+            "  \tShastri Nagar\n" +
+            "  \tShastri Park\n" +
+            "  \tShiv Vihar\n" +
+            "  \tShivaji Park\n" +
+            "  \tShivaji Stadium\n" +
+            "  \tShyam park\n" +
+            "  \tSikandarpur\n" +
+            "  \tSikandarpur (Rapid Metro)\n" +
+            "  \tSir Vishweshwaraiah Moti Bagh\n" +
+            "  \tSouth Extension\n" +
+            "  \tSubhash Nagar\n" +
+            "  \tSukhdev Vihar\n" +
+            "  \tSultanpur\n" +
+            "  \tSurajmal Stadium\n" +
+            "  \tTagore Garden\n" +
+            "  \tTerminal 1 IGI Airport\n" +
+            "  \tTikri Border\n" +
+            "  \tTikri Kalan\n" +
+            "  \tTilak Nagar\n" +
+            "  \tTis Hazari\n" +
+            "  \tTrilokpuri Sanjay Lake\n" +
+            "  \tTughlakabad\n" +
+            "  \tUdyog Bhawan\n" +
+            "  \tUdyog Nagar\n" +
+            "  \tUttam Nagar East\n" +
+            "  \tUttam Nagar West\n" +
+            "  \tVaishali\n" +
+            "  \tVasant Vihar\n" +
+            "  \tVidhan Sabha\n" +
+            "  \tVinobapuri\n" +
+            "  \tVinod Nagar East\n" +
+            "  \tVishwavidyalaya\n" +
+            "  \tWelcome\n" +
+            "  \tYamuna Bank"};
+
+    public  static  void main(String[] args){
+        System.out.println();
+    }
+}
---
+++ src/com/pluralsight/Flight.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+//checking method return value!
+public class Flight {
+    private int passengers;
+    private  int seats;
+
+    public boolean hasseats(Flight f2){
+        int total = passengers+f2.passengers;
+        return  total <= seats;
+    }
+
+    public Flight createnewwithboth(Flight f2) {
+        Flight newFlight = new Flight();
+        newFlight.seats = seats;
+        newFlight.passengers = passengers + f2.passengers;
+        return newFlight;
+    }
+
+    public void displaysomething(){
+        System.out.println("yes object created");
+    }
+
+    public  static void main(String[] args){
+        Flight flight1 = new Flight();
+        Flight flight2 = new Flight();
+        Flight flight3;
+
+        if (flight1.hasseats(flight2)){
+            flight3 = flight1.createnewwithboth(flight2);
+            flight3.displaysomething();
+            if (flight3 != null){
+                System.out.println("not null");
+            }
+        }
+
+    }
+
+}
+
+
